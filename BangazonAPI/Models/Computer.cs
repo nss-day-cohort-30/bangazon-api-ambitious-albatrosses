c496932b4d186a68c1f48ce88cb11a103ecb3ac4
Stephen_Clark <stephenc369@gmail.com>
2019-05-10 11:14:33 -0500

    add models

---
+++ BangazonAPI/Models/Computer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+    public class Computer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime PurchaseDate { get; set; }
+
+        public DateTime DecomssissionDate { get; set; }
+
+        [Required]
+        [StringLength(55)]
+        public string Make { get; set; }
+
+
+        [Required]
+        [StringLength(55)]
+
+        public string Manufacturer { get; set; }
+
+        
+    }
+    
+}
+
+
---
+++ BangazonAPI/Models/TrainingProgram.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+    public class TrainingProgram
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        [Required]
+        public DateTime StartDate { get; set; }
+        [Required]
+        public DateTime EndDate { get; set; }
+
+        public int MaxAtendees { get; set; }
+
+    }
+}
